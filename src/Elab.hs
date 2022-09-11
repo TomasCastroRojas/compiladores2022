5e27282e33feac837846fdf1a64d84dc64e16f4f
TomasCastro <tomascastrorojas18@gmail.com>
2022-09-10 21:59:37 -0300

    fix elaboracion de terminos y declaraciones, falta unificar estructura del codigo

--- src/Elab.hs
+++ src/Elab.hs
@@ -25,81 +25,76 @@ elab' :: MonadFD4 m =>  [Name] -> STerm -> m Term
 elab' env (SV p v) =
   -- Tenemos que ver si la variable es Global o es un nombre local
   -- En env llevamos la lista de nombres locales.
-  if v `elem` env 
+  if v `elem` env
     then  return $ V p (Free v)
     else return $ V p (Global v)
 
 elab' _ (SConst p c) = return $ Const p c
 
 
--- elab' env (SLam p [] t) = do 
---   t' <- elab' env t
---   return t'
+elab' env (SLam p [] t) = failPosFD4 p "Función sin argumentos"
+elab' env (SLam p [(v, ty)] t) = do
+  t' <- elab' (v:env) t
+  ty' <- elabTy ty
+  return $ Lam p v ty' (close v t')
 elab' env (SLam p ((v,ty):tl) t) = do
-  t' <- (elab' (v:env) (SLam p tl t))
+  t' <- elab' (v:env) (SLam p tl t)
   ty' <- elabTy ty
   return $ Lam p v ty' (close v t')
 
 
-
+elab' env (SFix p (f,fty) [] t) = failPosFD4 p "Fix sin argumentos"
 elab' env (SFix p (f,fty) [(x,xty)] t) = do
-  t' <- elab' (x:f:env) t
+  t'   <- elab' (x:f:env) t
   fty' <- elabTy fty
   xty' <- elabTy xty
-  return $ Fix p f fty' x xty' (close2 f x (t'))
+  return $ Fix p f fty' x xty' (close2 f x t')
 elab' env (SFix p (f, fty) ((x, xty):tl) t) = do
-  t' <- (elab' (x:f:env) (SLam p tl t))
+  t'   <- elab' (x:f:env) (SLam p tl t)
   fty' <- elabTy fty
   xty' <- elabTy xty
-  return $ Fix p f fty' x xty' (close2 f x t') 
+  return $ Fix p f fty' x xty' (close2 f x t')
 
 
 
-elab' env (SIfZ p c t e)         = do 
+elab' env (SIfZ p c t e)         = do
   cc <- elab' env c
   tt <- elab' env t
   ff <- elab' env e
   return $ IfZ p cc tt ff
 -- Operadores binarios
 elab' env (SBinaryOp i o t u) = do
-  t' <- (elab' env t) 
-  u' <- (elab' env u)
-  return $ BinaryOp i o t' u'
+  t' <- elab' env t
+  BinaryOp i o t' <$> elab' env u
 -- Operador Print
 elab' env (SPrint i str t) = do
-  t' <- (elab' env t)
-  return $ Print i str t'
+  Print i str <$> elab' env t
 -- Aplicaciones generales
-elab' env (SApp p h a) = do 
-  h' <- (elab' env h) 
-  a' <- (elab' env a)
-  return $ App p h' a'
+elab' env (SApp p h a) = do
+  h' <- elab' env h
+  App p h' <$> elab' env a
+
+elab' env (SLet _ p [] _ _) = failPosFD4 p "Let vacio"
+elab' env (SLet _ p [(name, ty)] def body) = do
+  def' <- elab' env def
+  ty' <- elabTy ty
+  body' <- elab' (name:env) body
+  return $ Let p name ty' def' (close name body')
+elab' env (SLet False p ((v,rty):xs) def body) = do
+  def' <- elab' (getVars xs ++ env) (SLam p xs def)
+  rty' <- elabTy $ buildFunTy xs rty
+  Let p v rty' def' . close v <$> elab' (v:env) body
+
+elab' env (SLet True p ((f,rty):xs) def body) = 
+  let rty' = buildFunTy xs rty in do
+    def' <- elab' env def
+    ty <- elabTy rty'
+    body' <- elab' (getVars xs ++ env) (SFix p (f, rty') xs body)
+    return $ Let p f ty def' (close f body')
 
 
-elab' env (SLet False p [(v,vty)] def body) = do
-  def' <- (elab' env def)
-  vty' <- (elabTy vty)
-  body'  <- (elab' (v:env) body)
-  return $ Let p v vty' def' (close v body')
-
--- elab' env (SLet False p ((f,fty):tl) def body) =  
---   Let p f fty (elab' env def) (close f (elab' (f:env) (SLam p tl body)))
-
-elab' env (SLet False p ((f,fty):tl) def body) = do
-  def' <- (elab' env (SLam p tl def))
-  fty' <- (elabTy fty)
-  body' <- (elab' (f:env) body)
-  return $ Let p f fty' def' (close f body')
-
-
--- elab' env (SLet p True [(v,vty)] def body) =  
---   Let p v vty (elab' env def) (close v (elab' (v:env) body))
-
-elab' env (SLet True p ((f,fty):tl) def body) = do
-  def' <- (elab' env def)
-  fty' <- (elabTy fty)
-  body' <- (elab' (f:env) (SFix p (f, fty) tl body))
-  return $ Let p f fty' def' (close f body')
+getVars :: [(Name, STy)] -> [Name]
+getVars = map fst 
 
 elabTy :: MonadFD4 m => STy -> m Ty
 elabTy SNatTy = return NatTy
@@ -110,14 +105,28 @@ elabTy (SFunTy t1 t2) = do
 elabTy (SNameTy name) = do
   tipo <- lookupTypeSin name
   case tipo of
-    Nothing -> failFD4 $ "Sinónimo de tipo no declarado " ++ (show name)
+    Nothing -> failFD4 $ "Sinónimo de tipo no declarado " ++ show name
     (Just t) -> return $ VarTy name t
 
 elabDecl :: MonadFD4 m => SDecl STerm -> m (Decl Term)
-elabDecl (SDecl pos name body) = do
+elabDecl (SDecl pos []  _ _) = failPosFD4 pos "Declaracion sin variables"
+elabDecl (SDecl pos [(n, sty)] body _) = do
   body' <- elab body
-  return $ Decl pos name body'
-elabDecl (SDeclTy pos name t) = do
-  ty <- elabTy t
+  ty <- elabTy sty
+  return $ Decl pos n ty body'
+elabDecl (SDecl pos ((n, sty):bs) body False) = do
+  body' <- elab (SLam pos bs body)
+  fty <- elabTy $ buildFunTy bs sty
+  return $ Decl pos n fty body'
+elabDecl (SDecl pos ((n, sty):bs) body True) = do
+  body' <- elab (SFix pos (n, buildFunTy bs sty) bs body)
+  ty <- elabTy $ buildFunTy bs sty
+  return $ Decl pos n ty body'
+elabDecl (SDeclSTy pos name sty) = do
+  ty <- elabTy sty
   addTypeSin (name, ty)
-  return $ Decl  pos name (V pos (Global name))
+  return $ Decl pos name ty (V pos (Global name))
+
+
+buildFunTy :: [(Name, STy)] -> STy -> STy
+buildFunTy xs sty = foldr1 SFunTy (map snd xs ++ [sty])
