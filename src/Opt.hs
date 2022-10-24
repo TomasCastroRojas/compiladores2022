32584e50dfb4eacc9888aac0120321ec00ae0a77
Alumno <tomascastrorojas18@gmail.com>
2022-10-24 16:18:49 -0300

    Modificada función visit

--- src/Opt.hs
+++ src/Opt.hs
@@ -1,8 +1,10 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Opt where
-import MonadFD4 (MonadFD4)
+import MonadFD4
 import Lang
 import Eval (semOp)
-import Subst (subst)
+import Subst (subst, open, close, open2, close2)
+import Control.Monad
 
 maxOpt :: Int
 maxOpt = 10
@@ -28,6 +30,61 @@ deadCode (IfZ i (Const i' (CNat 0)) t1 t2) = deadCode t1
 deadCode (IfZ i (Const i' (CNat n)) t1 t2) = deadCode t2
 deadCode t = return t
 
+inline :: MonadFD4 m => TTerm -> m TTerm
+inline  (App i (Lam i' name ty scope) nat@(Const i'' c)) = do
+    printFD4 "hola"
+    return $ subst nat scope
+inline  (App i (Lam i' name ty scope) t) = return (Let i name ty t scope)
+inline t = do
+    printFD4 "Chau"
+    return t
+
+freshen :: [Name] -> Name -> Name
+freshen ns n = let cands = n : map (\i -> n ++ show i) [0..] 
+               in head (filter (`notElem` ns) cands)
+
+
+visit :: MonadFD4 m => (TTerm -> m TTerm) -> TTerm -> [Name] -> m TTerm
+visit f v@(V i var) ns = f v
+visit f (Const i c) ns = return $ Const i c
+
+visit f (Lam i name ty t) ns = do
+    let n' = freshen ns name
+    t' <- visit f (open n' t) ns
+    f (Lam i name ty (close n' t'))
+
+
+visit f (App i l r) ns = do
+    l' <- visit f l ns
+    r' <- visit f r ns
+    f (App i l' r')
+
+visit f (Print i str t) ns = do
+    t' <- visit f t ns
+    f (Print i str t')
+
+visit f (BinaryOp i op t1 t2) ns = do
+    t1' <- visit f t1 ns
+    t2' <- visit f t2 ns
+    f (BinaryOp i op t1' t2')
+
+visit f (Fix i n1 t1 n2 t2 term) ns = do
+    let n1' = freshen ns n1
+    let n2' = freshen ns n2 
+    term' <- visit f (open2 n1' n2' term) ns
+    f (Fix i n1 t1 n2 t2 (close2 n1' n2' term'))
+
+visit f (IfZ info tc tt tf) ns = do
+    tc' <- visit f tc ns
+    tt' <- visit f tt ns
+    tf' <- visit f tf ns
+    f (IfZ info tc' tt' tf')
+
+visit f (Let i n ty def t) ns = do
+    def' <- visit f def ns
+    let n' = freshen ns n
+    body' <- visit f (open n' t) ns
+    f (Let i n ty def' (close n' body'))
 
 {-
 TODO >> Abstraccion de recorrido del AST, evita reescribir todos los casos de TTerm en cada funcion
@@ -47,7 +104,7 @@ br = visit br1 where
 
 optimizeN :: MonadFD4 m => Int -> TTerm -> m TTerm
 optimizeN 0 t = return t
-optimizeN n t = constantPropagation t >>= constantFolding >>= deadCode >>= optimizeN (n-1)
+optimizeN n t = visit (constantPropagation >=> constantFolding >=> deadCode >=> inline) t [] >>= optimizeN (n-1)
 
 optimize :: MonadFD4 m => Decl TTerm -> m (Decl TTerm)
 optimize (Decl p n ty tt) = Decl p n ty <$> optimizeN maxOpt tt
