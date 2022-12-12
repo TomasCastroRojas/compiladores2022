2f7d3df72735763395d585cacfe79d1f1e709c75
TomasCastro <tomascastrorojas18@gmail.com>
2022-12-12 14:47:31 -0300

    Intento de fix de optimizaciones, principal problema archivo test_opt0. Ver como expandir las definiciones globales

--- src/Opt.hs
+++ src/Opt.hs
@@ -6,16 +6,17 @@ import Eval (semOp)
 import Subst (subst, open, close, open2, close2)
 
 maxOpt :: Int
-maxOpt = 10
+maxOpt = 20
 
 constantFolding :: MonadFD4 m => TTerm -> m TTerm
 constantFolding (BinaryOp i Add t (Const i'' (CNat 0))) = constantFolding t
 constantFolding (BinaryOp i Add (Const i' (CNat 0)) t) = constantFolding t
 constantFolding (BinaryOp i Sub t (Const i'' (CNat 0))) = constantFolding t
-constantFolding (BinaryOp i Sub (Const i' (CNat 0)) t) =
-    return $ Const i (CNat 0)
-constantFolding (BinaryOp i op (Const i' (CNat n)) (Const i'' (CNat m))) =
-    return $ Const i (CNat (semOp op n m))
+constantFolding (BinaryOp i Sub (Const i' (CNat 0)) t) = return $ Const i (CNat 0)
+constantFolding (BinaryOp i op (Const i' (CNat n)) (Const i'' (CNat m))) = return $ Const i (CNat (semOp op n m))
+constantFolding (BinaryOp i Add (BinaryOp i' Add t (Const i0 (CNat n))) (Const i1 (CNat m))) = return $ BinaryOp i Add t (Const i0 (CNat (n+m)))
+constantFolding (IfZ i (Const i' (CNat 0)) t1 t2) = constantFolding t1
+constantFolding (IfZ i (Const i' (CNat n)) t1 t2) = constantFolding t2
 constantFolding t = return t
 
 constantPropagation :: MonadFD4 m => TTerm -> m TTerm
@@ -23,13 +24,8 @@ constantPropagation (Let info name ty nat@(Const i (CNat n)) scope) =
     return $ subst nat scope
 constantPropagation t = return t
 
-
-deadCode :: MonadFD4 m => TTerm -> m TTerm
-deadCode (IfZ i (Const i' (CNat 0)) t1 t2) = deadCode t1
-deadCode (IfZ i (Const i' (CNat n)) t1 t2) = deadCode t2
-deadCode t = return t
-
 betaRedex :: MonadFD4 m => TTerm -> m TTerm
+betaRedex  (App i (Lam i' name ty scope) val@(Const _ _)) = return $ subst val scope
 betaRedex  (App i (Lam i' name ty scope) t) = return (Let i name ty t scope)
 betaRedex t = return t
 
@@ -81,25 +77,9 @@ visit f (Let i n ty def t) ns = do
     body' <- visit f (open n' t) (n':ns)
     f (Let i n ty def' (close n' body'))
 
-{-
-TODO >> Abstraccion de recorrido del AST, evita reescribir todos los casos de TTerm en cada funcion
-
-
-visit :: Monad m => (TTerm -> m TTerm) -> TTerm -> m TTerm
-visit f (App i l r) = do
-    l' <- visit f l
-    r' <- visit f r
-    f (App i l' r')
-
-
-br = visit br1 where
-    br1 (App (Lam ...) v) = algo 
-    br1 t = return t
--}
-
 optimizeN :: MonadFD4 m => Int -> TTerm -> m TTerm
 optimizeN 0 t = return t
-optimizeN n t = visit (constantPropagation >=> constantFolding >=> deadCode >=> betaRedex >=> inline) t [] >>= optimizeN (n-1)
+optimizeN n t = visit (constantPropagation >=> constantFolding >=> betaRedex >=> inline) t [] >>= optimizeN (n-1)
 
 optimize :: MonadFD4 m => Decl TTerm -> m (Decl TTerm)
 optimize (Decl p n ty tt) = Decl p n ty <$> optimizeN maxOpt tt
